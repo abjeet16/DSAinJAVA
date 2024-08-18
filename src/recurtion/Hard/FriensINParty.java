2f5f0df6610a6459ada36eaee071f7a84a3af124
abjeet16 <avjeetyadav00@gmail.com>
2024-08-18 12:58:25 +0530

    doing

--- src/recurtion/Hard/FriensINParty.java
+++ src/recurtion/Hard/FriensINParty.java
@@ -7,15 +7,17 @@ public class FriensINParty {
     }
 
     private static int inviteFriends(int numberOfFriends) {
+        //if there is only one friend left then he can be only called alone so return 1
         if(numberOfFriends == 1){
             return 1;
         }
+        //if there are only 2 friends left then they can only be called 1-1 or in a pair of 1 so return 2
         if (numberOfFriends == 2){
             return 2;
         }
         // if called in Alone
         int alone = inviteFriends(numberOfFriends-1);
-        // if called in pair
+        // if called in pair (numberOFFriends-1)*inviteFriend because he can choose any one friend as a pair from the remaining n-1 Friends
         int pair = (numberOfFriends-1)*inviteFriends(numberOfFriends-2);
 
         return alone+pair;
