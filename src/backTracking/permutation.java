5e018c93e232fd183230fca86b88c2d122c081cb
abjeet16 <avjeetyadav00@gmail.com>
2024-09-01 20:48:11 +0530

    doing

---
+++ src/backTracking/permutation.java
@@ -0,0 +1,19 @@
+package backTracking;
+
+public class permutation {
+    public static void main(String[] args) {
+        String str = "ABC";
+        printALlPermutation(str,"",0);
+    }
+    static void printALlPermutation(String str,String permutation,int index){
+        if (str.isEmpty()){
+            System.out.println(permutation);
+            return;
+        }
+        for (int i = 0; i < str.length();i++){
+            char currentChar = str.charAt(i);
+            String newString = str.substring(0,i)+str.substring(i+1);
+            printALlPermutation(newString,permutation+currentChar,index+1);
+        }
+    }
+}
