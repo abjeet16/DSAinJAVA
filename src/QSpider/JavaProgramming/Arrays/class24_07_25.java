6f1eed9db325e3fc59c11e501a52313cfa4afe55
abjeet16 <avjeetyadav00@gmail.com>
2025-07-24 14:45:04 +0530

    done

--- src/QSpider/JavaProgramming/Arrays/class24_07_25.java
+++ src/QSpider/JavaProgramming/Arrays/class24_07_25.java
@@ -4,15 +4,52 @@ import static QSpider.JavaProgramming.Arrays.class18_07_25.printArr;
 
 public class class24_07_25 {
     public static void main(String[] args) {
-        int[] arr = {2,3,5,3,3,3,6,7,8,9,6,7};
+        int[] arr = {2,3,5,6,7,8,9,10,45,77};
         System.out.println("Original Array");
         printArr(arr);
         int element = 10;
         /**
          * for leanear search
          */
-        linearSearch(arr,element);
+        //linearSearch(arr,element);
+        /**
+         * binary search
+         */
+        //binarySearch(arr,element);
     }
+
+    /*private static void binarySearch(int[] arr, int element) {
+        if (arr.length/2!=0)
+            if (arr[arr.length/2+1]==element){
+                System.out.println("fount at index : "+(arr.length/2+1));
+                return;
+            }
+
+
+        for (int i = 0 ; i < arr.length/2;i++){
+            if (arr[i]==element){
+                System.out.println("fount at index : "+i);
+                return;
+            } else if (arr[arr.length-1-i]==element) {
+                System.out.println("fount at index : "+(arr.length-1-i));
+                return;
+            }
+        }
+        System.out.println("not found");
+        int first = 0,last = arr.length-1;
+        while (first<=last){
+            if (arr[first]==element){
+                System.out.println("fount at index : "+first);
+                return;
+            } else if (arr[last]==element) {
+                System.out.println("fount at index : "+last);
+                return;
+            }
+            first++;
+            last--;
+        }
+        System.out.println("not found");
+    }*/
 
     private static void linearSearch(int[] arr, int n) {
         for (int i = 0 ; i < arr.length ; i ++){
@@ -22,5 +59,21 @@ public class class24_07_25 {
             }
         }
         System.out.println("not found ");
+    }
+    static int binarySearch(int[] arr, int element){
+        int start = 0;
+        int end = arr.length;
+        while (start<=end) {
+            int mid = (start+end)/ 2;
+            if (arr[mid] == element)
+                return mid;
+            else if (arr[mid] > element) {
+                end = mid-1;
+            }
+            else {
+                start = mid+1;
+            }
+        }
+        return -1;
     }
 }
---
+++ src/QSpider/JavaProgramming/String/class24_07_25.java
@@ -0,0 +1,41 @@
+package QSpider.JavaProgramming.String;
+
+public class class24_07_25 {
+    public static void main(String[] args) {
+        /**
+         * count the number char leaving space
+         */
+        //numberOfCharInString("hi i am abjeet");
+        /**
+         * rev string
+         */
+        //revString("abjeet");
+        /**
+         *
+         */
+        checkPalString("abjajba");
+    }
+
+    private static void checkPalString(String string) {
+        for (int i = 0; i < string.length()/2;i++)
+            if (string.charAt(i)!=string.charAt(string.length()-1-i)){
+                System.out.println("no");
+                return;
+            }
+        System.out.println("yes");
+    }
+
+    private static void numberOfCharInString(String message) {
+        int count = 0;
+        for (int i=0;i<message.length();i++)
+            if (message.charAt(i)!=' ')
+                count++;
+        System.out.println(count);
+    }
+    private static void revString(String message) {
+        String rev="";
+        for (int i=message.length()-1;i>=0;i--)
+            rev +=message.charAt(i);
+        System.out.println(rev);
+    }
+}
