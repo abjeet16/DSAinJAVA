a0179ca2fc2f11ce739ecbdbf4e8604ef31ed73f
abjeet16 <avjeetyadav00@gmail.com>
2025-08-03 16:22:53 +0530

    done

---
+++ src/Practice/functionalInerface.java
@@ -0,0 +1,14 @@
+package Practice;
+
+public class functionalInerface {
+    public static void main(String[] args) {
+        Functional functional = (num1,num2)->{
+            System.out.println(num1+num2);
+        };
+        functional.addNumber(10,20);
+    }
+}
+@FunctionalInterface
+interface Functional{
+    void addNumber(int num1 , int num2);
+}
