8533a1344ee40151114d35b75f8fb9a96553bce1
abjeet16 <avjeetyadav00@gmail.com>
2025-09-22 13:42:37 +0530

    done

---
+++ src/leetCode/easy/CountElementsWithMaximumFrequency.java
@@ -0,0 +1,19 @@
+package leetCode.easy;
+
+public class CountElementsWithMaximumFrequency {
+    public int maxFrequencyElements(int[] nums) {
+        int[] freq = new int[101];
+        int maxFreq = 0;
+        int result = 0;
+        for (int i = 0 ; i < nums.length ; i ++){
+            freq[nums[i]]++;
+            if (maxFreq<freq[nums[i]])
+                maxFreq = freq[nums[i]];
+        }
+        for (int i = 0 ; i < freq.length ; i++){
+            if (freq[i]==maxFreq)
+                result+=maxFreq;
+        }
+        return result;
+    }
+}
