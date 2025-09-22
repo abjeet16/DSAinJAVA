6cf0b75275cc990a0c47735c8cb90a9833498e69
abjeet16 <avjeetyadav00@gmail.com>
2025-09-22 13:30:27 +0530

    done

---
+++ src/leetCode/mid/LongestMountainInArray.java
@@ -0,0 +1,12 @@
+package leetCode.mid;
+
+public class LongestMountainInArray {
+    public int longestMountain(int[] arr) {
+        if (arr.length<3)
+            return 0;
+        int result = 0 ;
+        int mountainStart = 0,mountainEnd = 1;
+        boolean upHill = false;
+
+    }
+}
--- src/leetCode/mid/ThreeSum.java
+++ src/leetCode/mid/ThreeSum.java
@@ -1,13 +1,10 @@
 package leetCode.mid;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 public class ThreeSum {
-    public static List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
+    public static List<List<Integer>> threeSumMyWay(int[] nums) {
+        Set<List<Integer>> result = new HashSet<>();
         for (int i = 0 ; i < nums.length-2 ; i ++){
             HashMap<Integer,Integer> hashMap = new HashMap<>();
             for (int j = i+1;j<nums.length;j++){
@@ -16,17 +13,45 @@ public class ThreeSum {
                     list.add(nums[i]);
                     list.add(hashMap.get(nums[j]));
                     list.add(nums[j]);
-
+                    Collections.sort(list);
                     result.add(list);
                 }else
                     hashMap.put(-(nums[i]+nums[j]),nums[j]);
             }
-            System.out.println(hashMap);
         }
-        return result;
+        return result.stream().toList();
+    }
+
+    public static List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue; // skip duplicate i
+
+            int l = i + 1, r = nums.length - 1;
+            while (l < r) {
+                int sum = nums[i] + nums[l] + nums[r];
+
+                if (sum == 0) {
+                    res.add(Arrays.asList(nums[i], nums[l], nums[r]));
+
+                    // move l and r past duplicates
+                    int lv = nums[l], rv = nums[r];
+                    while (l < r && nums[l] == lv) l++;
+                    while (l < r && nums[r] == rv) r--;
+                } else if (sum < 0) {
+                    l++;
+                } else {
+                    r--;
+                }
+            }
+        }
+        return res;
     }
 
     public static void main(String[] args) {
+        //System.out.println(threeSum(new int[]{-1,0,1,2,-1,-4}));
         System.out.println(threeSum(new int[]{-1,0,1,2,-1,-4}));
     }
 }
