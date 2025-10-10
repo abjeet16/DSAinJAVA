a02a55a6d8c49389092624db3fc4848e0dbb48f6
abjeet16 <avjeetyadav00@gmail.com>
2025-10-10 12:30:19 +0530

    done

--- src/leetCode/mid/array/TakingMaximumEnergyFromTheMysticDungeon.java
+++ src/leetCode/mid/array/TakingMaximumEnergyFromTheMysticDungeon.java
@@ -2,13 +2,11 @@ package leetCode.mid.array;
 
 public class TakingMaximumEnergyFromTheMysticDungeon {
     public int maximumEnergy(int[] energy, int k) {
-        for (int i = energy.length-1-k; i >= 0;i--){
+        for (int i = energy.length-1-k; i >= 0;i--)
             energy[i] = energy[i+k]+energy[i];
-        }
         int max =  energy[0];
         for (int i : energy)
-            if (max<i)
-                max = i;
+            max = Math.max(max,i);
         return max;
     }
 }
