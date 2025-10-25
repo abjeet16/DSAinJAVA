52cbb0648ad7f7ed5c4f6a414915d02a457951af
abjeet16 <avjeetyadav00@gmail.com>
2025-10-25 16:50:08 +0530

    done

--- src/leetCode/easy/math/CalculateMoneyInLeetcodeBank.java
+++ src/leetCode/easy/math/CalculateMoneyInLeetcodeBank.java
@@ -2,7 +2,20 @@ package leetCode.easy.math;
 
 /// 1716. Calculate Money in Leetcode Bank
 public class CalculateMoneyInLeetcodeBank {
-    public int totalMoney(int n) {
+    public static int totalMoney(int n) {
+        int base = 0,res = 0;
+        while (n!=0){
+            int limit = Math.min(7,n);
+            for (int i = 1 ; i <=  limit; i++){
+                res+=base+i;
+            }
+            n-=limit;
+            base++;
+        }
+        return res;
+    }
 
+    public static void main(String[] args) {
+        System.out.println(totalMoney(10));
     }
 }
---
+++ src/leetCode/mid/array/binarySearch/KokoEatingBananas.java
@@ -0,0 +1,46 @@
+package leetCode.mid.array.binarySearch;
+
+/// 875. Koko Eating Bananas
+public class KokoEatingBananas {
+    public static int minEatingSpeed(int[] piles, int h) {
+        int right = getMaxValue(piles);
+        int left = 1 , res = 0;
+        while (left<=right){
+            int mid = (left+right)/2;
+            if (canHeEat(mid,piles,h)){
+                res = mid;
+                right = mid-1;
+            }else {
+                left = mid+1;
+            }
+        }
+        return res;
+    }
+
+    private static int getMaxValue(int[] piles) {
+        int max = Integer.MIN_VALUE;
+        for (int ele : piles)
+            max = Math.max(ele,max);
+        return max;
+    }
+
+    private static boolean canHeEat(int eatPerHour, int[] piles, int maxHours) {
+        int hoursTaken = 0;
+        for (int ele : piles){
+            hoursTaken+= ele / eatPerHour;
+            if (ele %eatPerHour!=0){
+                hoursTaken++;
+            }
+            if (hoursTaken>maxHours)
+                return false;
+        }
+        System.out.println(eatPerHour+" : "+(hoursTaken<=maxHours)+" ( "+hoursTaken+" )");
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int[] piles = {805306368,805306368,805306368};
+        int h = 1000000000;
+        System.out.println(minEatingSpeed(piles,h));
+    }
+}
