738d4d4c2c845f92176ebc4faf33d0375e1ba5dc
abjeet16 <avjeetyadav00@gmail.com>
2025-11-19 22:44:10 +0530

    done

--- src/leetCode/strivers/arrays/mid/SetMatrixZeroes.java
+++ src/leetCode/strivers/arrays/mid/SetMatrixZeroes.java
@@ -1,6 +1,9 @@
 package leetCode.strivers.arrays.mid;
 
+
 import java.util.Arrays;
+
+import static leetCode.easy.Arrays.MajorityElement.print;
 
 /// 73. Set Matrix Zeroes
 public class SetMatrixZeroes {
@@ -28,37 +31,40 @@ public class SetMatrixZeroes {
     public static void setZeroesBetter(int[][] matrix){
         int m = matrix.length;
         int n = matrix[0].length;
+        boolean firstRowHasZero = false;
+        boolean firstColHasZero = false;
         for (int i = 0; i < m; i++) {
             if (matrix[i][0] == 0) {
-                matrix[0][0] = 0;
+                firstRowHasZero=true;
                 break;
             }
         }
         for (int i = 1; i < n; i++) {
             if (matrix[0][i]==0){
-                matrix[0][0]=0;
+                firstColHasZero=true;
                 break;
             }
         }
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                if (matrix[i][j]==0){
-                    matrix[i][0]=0;
-                    matrix[0][j]=0;
-                }
-            }
-        }
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
+
+
+        print(matrix);
+
+        for (int i = m-1; i >= 0; i--) {
+            for (int j = n-1; j >= 0; j--) {
                 if (matrix[i][0]==0||matrix[0][j]==0)
                     matrix[i][j]=0;
             }
         }
     }
 
+    private static void print(int[][] matrix) {
+        for (int[] row : matrix)
+            System.out.println(Arrays.toString(row));
+    }
+
     public static void main(String[] args) {
         //int[][] mat = {{0,1,2,0},{3,4,5,2},{1,3,1,5}};
         int[][] mat = {{1,2,3,4},{5,0,7,8},{0,10,11,12},{13,14,15,0}};
-        setZeroes(mat);
+        setZeroesBetter(mat);
     }
 }
